6c78599af31eaec731315d0da7aef095f2272bb7
arkhasiaa <depthofmysea@gmail.com>
2021-06-04 15:17:22 +0300

    Add cells

--- new_1.js
+++ new_1.js
@@ -13,7 +13,7 @@ for (let index = 0; index < 2000; index++) {
     const element = document.createElement('div');
     element.classList.add("item");
     // element.innerHTML = index;
-    element.addEventListener("mouseenter", function () {
+    element.addEventListener("click", function () {
         // element.style.backgroundColor = "black";
         var imageUrl = "url(" + randomArrayItem(imgArray) + ")";
         console.log(imageUrl);
@@ -48,16 +48,49 @@ for (var i = 0; i < 20; i++) {
 
 // setTimeout(reset, 5000);
 
+// function reset() {
+//     var items = document.querySelectorAll (".item");
+//     items.forEach(function (item){
+//         item.style.backgroundImage = "none";
+//     });
+
+// }
+
 // setInterval(reset, 5000);
 
-setInterval (function () {
-    var items = Array.from (document.querySelectorAll (".item")).slice(500, 900);
-    var randomItem = randomArrayItem(items);
-    randomItem.style.backgroundImage = "url(" + imgArray[0] + ")";
-}, 10);
+// setInterval (function () {
+//     var items = Array.from (document.querySelectorAll (".item")).slice(500, 900);
+//     var randomItem = randomArrayItem(items);
+//     randomItem.style.backgroundImage = "url(" + imgArray[0] + ")";
+// }, 10);
 
-setInterval (function () {
-    var items = Array.from (document.querySelectorAll (".item")).slice(1000, 2000);
-    var randomItem = randomArrayItem(items);
-    randomItem.style.backgroundImage = "url(" + imgArray[5] + ")";
-}, 100);
+// setInterval (function () {
+//     var items = Array.from (document.querySelectorAll (".item")).slice(1000, 2000);
+//     var randomItem = randomArrayItem(items);
+//     randomItem.style.backgroundImage = "url(" + imgArray[5] + ")";
+// }, 100);
+
+
+var cells = document.querySelectorAll('.cell');
+var cellsToFill = []
+cellsToFill.forEach(function(cellIndex){
+    var cell = cells[cellIndex]
+    cell.style.backgroundImage = ''
+});
+
+cells.forEach(function(cell, cellIndex) {
+    cell.addEventListener('click', function () {
+        // fill the cell
+        cellsToFill.push(cellIndex)
+    })
+});
+
+console.log(cellsToFill)
+
+var cosoleButton = document.querySelector(".console-button");
+cosoleButton.addEventListener("click", function () {
+    var p = document.querySelectorAll(".push");
+    push.forEach(function (item) {
+        push.style.backgroundColor= "black";
+    });
+});
